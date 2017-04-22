e37478848f719eaedb351f36b2ceb748533646d7
Admin <admin@Mac-Admin.local>
2017-04-22 10:38:42 +0300

    Initial Commit

---
+++ Task_on_practis/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Task_on_practis
+//
+//  Created by Admin on 22.04.17.
+//  Copyright © 2017 Daniil Boyko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
